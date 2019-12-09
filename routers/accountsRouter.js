8c02d7966727a80691c802cba7a059d976b7c954
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-12-09 17:32:13 -0600

    build endpoint for update account

--- routers/accountsRouter.js
+++ routers/accountsRouter.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
+// Get all accounts
 router.get("/", (req, res) => {
   knex("accounts")
     .select("*")
@@ -15,6 +16,24 @@ router.get("/", (req, res) => {
       });
     });
 });
+
+// Get account by id
+router.get("/:id", (req, res) => {
+  knex("accounts")
+    .select("*")
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "error getting accounts"
+      });
+    });
+});
+
+// Adding new account
 
 router.post("/", (req, res) => {
   const newAccount = req.body;
@@ -32,6 +51,33 @@ router.post("/", (req, res) => {
     .catch(error => {
       res.status(500).json({
         errorMessage: "error getting accounts"
+      });
+    });
+});
+
+// Update account
+router.put("/:id", (req, res) => {
+  knex("accounts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(ids => {
+      console.log(ids);
+      knex("accounts")
+        .select("*")
+        .where({ id: ids })
+        .first()
+        .then(ccount => {
+          res.status(200).json(ccount);
+        })
+        .catch(error => {
+          res.status(500).json({
+            errorMessage: "error getting account"
+          });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "error update account"
       });
     });
 });
